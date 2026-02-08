47eb0cf421291db0406b41d2dae239d65249ad10
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2026-02-08 20:54:32 +0800

    Update VIRTUAL_SYSTEM_TYPE for compatibility

--- debian-ova-creator.sh
+++ debian-ova-creator.sh
@@ -121,7 +121,7 @@ DEBIAN_ARCH="${debarch}"
 # 7.0 -> vmx-17
 # 8.0 -> vmx-19
 # VIRTUAL_SYSTEM_TYPE="vmx-19" # vmx-19 适用于 vSphere 7.0 及以上版本
-VIRTUAL_SYSTEM_TYPE="vmx-4" # vmx-14 适用于 esxi6.7
+VIRTUAL_SYSTEM_TYPE="vmx-14" # vmx-14 适用于 esxi6.7
 
 FILE_NAME="debian-${DEBIAN_VERSION}-genericcloud-${DEBIAN_ARCH}"
 FILE_ORIG_EXT="qcow2"
